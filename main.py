2489c326d2f74e51c0cb22cb42fca988417bf75a
jessebrusa <jessebrusadev@gmail.com>
2023-07-17 23:10:48 -0400

    built out the search song page and the functions to find the lyrics, audio, and chords then redirect to its page

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, request
 from img_search import get_google_img
 from youtubesearchpython import VideosSearch
 from pytube import YouTube
+from chords import Chords
+import glob
+import shutil
+import azapi
 import certifi
 import os
 
 
+GUITAR_EMAIL = 'jessebrusa@gmail.com'
+GUITAR_PASSWORD = '1JesusKing7'
+GOOGLE_API_KEY = "AIzaSyBtuoL2-dL71kSmh6sPsrLSSpgn1thUYJg"
+GOOGLE_CX = "15d5cf5fb6e3c484c"
+
+
 app = Flask(__name__)
+
+
+def obtain_lyrics_create_dir(input_title):
+    path = "./static/music/"
+    API = azapi.AZlyrics('google', accuracy=0.5)
+    API.title = input_title
+
+    API.getLyrics()
+
+    # Come back to make file exist error catch and route to song-page
+    os.mkdir(f'{path}{API.title.lower()}')
+    API.getLyrics(save=True, path=f'{path}{API.title.lower()}')
+    os.rename(f'{path}{API.title.lower()}/{API.title} - {API.artist}.txt', 
+              f'{path}{API.title.lower()}/{API.title.lower()} - {API.artist.lower()}.txt')
+
+    img_url = get_google_img(f'{API.title} {API.artist}', GOOGLE_API_KEY, GOOGLE_CX)
+
+    with open(f'{path}{API.title.lower()}/info.txt', 'w') as file:
+        file.write(f'{API.title}\n{API.artist}\n{img_url}')
+
+    return [API.title, API.artist]
 
 
 def download_song(song_title, song_artist, path):
@@ -19,7 +50,6 @@ def download_song(song_title, song_artist, path):
     youtube = YouTube(video_url)
     audio_stream = youtube.streams.filter(only_audio=True).first()
     audio_stream.download(output_path=path, filename=f'{song_title.lower()}.mp3')
-
 
 
 def search_song_karaoke(song):
@@ -36,6 +66,16 @@ def search_song_karaoke(song):
         
     except FileExistsError:
         print('No Karaoke File Found')
+
+
+def move_from_downloads(destination_folder):
+    downloads_folder = '/Users/jessebrusa/Downloads'
+    files = glob.glob(os.path.join(downloads_folder, '*'))
+    sorted_files = sorted(files, key=os.path.getmtime, reverse=True)
+    most_recent_file = sorted_files[0]
+
+    destination_path = os.path.join(destination_folder, os.path.basename(most_recent_file))
+    shutil.move(most_recent_file, destination_path)
 
 
 @app.route('/')
@@ -108,14 +148,35 @@ def song_page(song):
                            song=song)
 
 
-@app.route('/karaoke/<song>')
+@app.route('/karaoke/<string:song>')
 def get_karaoke(song):
     search_song_karaoke(song)
     return redirect(url_for('song_page', song=song))
 
 
-@app.route('/find-song')
+@app.route('/find-song', methods=['GET', 'POST'])
 def find_song():
+    if request.method == 'POST':
+        input_title = request.form.get('title')
+
+        song_data = obtain_lyrics_create_dir(input_title)
+        title = song_data[0]
+        artist = song_data[1]
+        path = f'./static/music/{title.lower()}/'
+
+        download_song(title, artist, path)
+
+        try:
+            chords = Chords()
+            chords.login_account(GUITAR_EMAIL, GUITAR_PASSWORD)
+            chords.search_song(title)
+            chords.save_pdf()
+            move_from_downloads(path)
+        except:
+            pass
+
+        return redirect(url_for('song_page', song=title.lower()))        
+
     return render_template('find-song.html')
 
 
